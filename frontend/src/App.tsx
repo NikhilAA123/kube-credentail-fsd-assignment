4fb64d8a7e85471e51ee902eb9769e0b600e2a55
Your Name~Nikhil <nikhilpantam79@gmail.com>
2025-10-11 01:35:11 +0530

    feat: build initial app layout with sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,28 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Flex, VStack, Heading, Link, Text } from "@chakra-ui/react";
 
 function App() {
   return (
-    <Box>
-      <Heading>Hello Chakra</Heading>
-    </Box>
+    <Flex h="100vh" w="100vw">
+      {/* Sidebar */}
+      <Box w="250px" bg="gray.800" color="white" p={5}>
+        <VStack align="stretch" spacing={8}>
+          <Heading size="md" as="h1">
+            Creds-Manager
+          </Heading>
+          <VStack align="stretch" spacing={4}>
+            <Link>Issue Credential</Link>
+            <Link>Verify Credential</Link>
+          </VStack>{" "}
+          {/* <-- Corrected this line */}
+        </VStack>
+      </Box>
+
+      {/* Main Content */}
+      <Box flex="1" p={10}>
+        <Heading>Welcome to Creds-Manager</Heading>
+        <Text mt={4}>Select an option from the sidebar to begin.</Text>
+      </Box>
+    </Flex>
   );
 }
 
