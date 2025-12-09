615d5adeb0711781cab71e8151fe5b9a6f8eaf7b
Your Name~Nikhil <nikhilpantam79@gmail.com>
2025-12-09 19:42:41 +0530

    implement auth request DTOs

---
+++ packages/authservice-java/src/main/java/com/kubecredential/auth/dto/TokenVerifyRequest.java
@@ -0,0 +1,13 @@
+package com.kubecredential.auth.dto;
+
+public class TokenVerifyRequest {
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
